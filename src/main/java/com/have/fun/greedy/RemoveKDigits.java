fc026c968db34eed77b72cbef4ed71efa29c815f
leih <leiheng_h@163.com>
2019-10-28 19:07:49 +0800

    error answer

--- src/main/java/com/have/fun/greedy/RemoveKDigits.java
+++ src/main/java/com/have/fun/greedy/RemoveKDigits.java
@@ -1,19 +1,36 @@
 package com.have.fun.greedy;
 
 public class RemoveKDigits {
-  public String removeKdigits(String num, int k) {
+  public String removeKDigits(String num, int k) {
     if (num.length() <= k) {
       return "0";
     }
     StringBuffer stringBuffer = new StringBuffer();
-    if (num.charAt(0) == '1' && num.charAt(1) !='0') {
-      stringBuffer.append(num.charAt(0));
-      stringBuffer.append(num.substring(k));
-    }else {
-      for(int i=0;i<num.length();i++){
-
+    int mn=num.charAt(0);
+    int mnIndex=0;
+    int zeroCun=0;
+    for(int i =1; i<= k;i++){
+      if (num.charAt(i) == 0) {
+        zeroCun++;
+      }
+      if (num.charAt(i) < mn) {
+        mn=num.charAt(i);
+        mnIndex=i;
       }
     }
+    if(zeroCun>0){
+      stringBuffer.append(num.substring(k + zeroCun));
+      System.out.println("first:"+stringBuffer.toString());
+      for(int i=0;i<stringBuffer.length();i++){
+        if(stringBuffer.charAt(i)!=0){
+          return stringBuffer.toString();
+        }
+      }
+      return "0";
+    }
+
+    stringBuffer.append(num.charAt(mnIndex));
+    stringBuffer.append(num.substring(k+1));
     return stringBuffer.toString();
   }
 }
