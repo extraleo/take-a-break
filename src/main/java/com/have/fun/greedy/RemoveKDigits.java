ed91fa0c7a0a9752d79a72fe573b68376c2b1839
leih <leiheng_h@163.com>
2019-10-30 11:45:40 +0800

    :dizzy_face:

--- src/main/java/com/have/fun/greedy/RemoveKDigits.java
+++ src/main/java/com/have/fun/greedy/RemoveKDigits.java
@@ -2,6 +2,14 @@ package com.have.fun.greedy;
 
 import java.util.Stack;
 
+/**
+ * create on 19-10-27
+ * LeetCode link: https://leetcode.com/problems/remove-k-digits/
+ *
+ * 这道题做的心情很复杂, 基本上想的思路都是有问题的.
+ * 看了讨论区的做法.
+ * 中心思想就是尽可能移除高位K个最大的数,使数字变小. [我第三个思路就是往这边想的, 因为前一个死在了 112,1 上.但是没想到用stack 做辅助.]
+ */
 public class RemoveKDigits {
   public String removeKDigits(String num, int k) {
     if (k == num.length()) {
@@ -9,8 +17,30 @@ public class RemoveKDigits {
     }
     Stack<Character> stack=new Stack<Character>();
     int i=0;
-    while (i < num.length()) {
-      while(k>0 )
+    int length = num.length();
+
+    while (i < length) {
+      while (k > 0 && !stack.empty() && stack.peek() > num.charAt(i)) {
+        stack.pop();
+        k--;
+      }
+      stack.push(num.charAt(i));
+      i++;
+      // "11111"
+      while (k > 0) {
+        stack.pop();
+        k--;
+      }
     }
+    StringBuilder builder = new StringBuilder();
+    while (!stack.isEmpty()) {
+      builder.append(stack.pop());
+    }
+    // 翻转
+    builder.reverse();
+    //remove all the 0 at the head
+    while(builder.length()>1 && builder.charAt(0)=='0')
+      builder.deleteCharAt(0);
+    return builder.toString();
   }
 }
